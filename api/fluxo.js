1400fc420e5a505ab970fcf650e8aceb5e3d3d50
gustavosilveira-ui <gustavosilveira@gpsbi.com.br>
2026-03-27 14:53:45 -0300

    Update fluxo.js

--- api/fluxo.js
+++ api/fluxo.js
@@ -1,7 +1,8 @@
 export default async function handler(req, res) {
-  const url =
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRTsj89x1ZL_Cyx-UsXMxxg-p9eSiioVzvGA4KhOzVq6Umq2sgjma1eGXsxSe8zf--JyNkOFJOFNzyy/pub?gid=706534493&single=true&output=csv";
 
+  const url =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRTsj89x1ZL_Cyx-UsXMxxg-p9eSiioVzvGA4Kh0zVq6Umg2sgjma1eGXsxSe8zf--JyNkOFJOFNzyy/pub?gid=706534493&single=true&output=csv";
+  
   try {
     const response = await fetch(url);
     const csv = await response.text();
